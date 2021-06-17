cd962be15440cb8446c79abb5950e4991faa8a80
Alex <zall99971@gmail.com>
2021-06-17 21:25:47 +0300

    Домашняя работа

--- src/hw1/HomeWorkApp.java
+++ src/hw1/HomeWorkApp.java
@@ -38,10 +38,10 @@ public class HomeWorkApp {
 //Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”,
 //если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”, если больше 100 (100 исключительно) - “Зеленый”;
     public static void printColor() {
-        int value = (int) Math.random ();
-        if (value <=0) System.out.println ("Красный");
-        if (value >0&& value<=100) System.out.println ("Желтый");
-        else System.out.println ("Зеленый");
+        int value = (int) (Math.random () * 200 - 50);
+        if (value <= 0) System.out.println ("Красный");
+        if (value > 0 && value <= 100) System.out.println ("Желтый");
+        else if (value >= 100)System.out.println ("Зеленый");
     }
     //Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями, которыми захотите.
     // Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
--- src/hw1/MainClass.java
+++ src/hw1/MainClass.java
@@ -2,6 +2,5 @@ package hw1;
 
 public class MainClass {
     public static void main(String[] args) {
-
     }
 }
